4ebef5ed19346d791a370a61523730e23d740bef
amir hossein <ahns3.ahe@gmail.com>
2018-09-18 16:18:25 +0430

    WordCount code added

---
+++ wordcount.py
@@ -0,0 +1,20 @@
+from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile("hello")
+
+class MRWordFreqCount(MRJob):
+
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def combiner(self, word, counts):
+        yield (word, sum(counts))
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+
+if __name__ == '__main__':
+    MRWordFreqCount.run()
