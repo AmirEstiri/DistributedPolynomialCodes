e41c5dcd2fc4103d810adead8510e49f2bfb96a3
amir hossein <ahns3.ahe@gmail.com>
2018-09-22 19:50:55 +0330

    Add letter count example for pyspark

---
+++ PySpark/example.py
@@ -0,0 +1,7 @@
+from pyspark import SparkContext
+sc = SparkContext("local", "example")
+logFile = "README.md"
+logData = sc.textFile(logFile).cache()
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
+print "Lines with a: %i, lines with b: %i" % (numAs, numBs)
