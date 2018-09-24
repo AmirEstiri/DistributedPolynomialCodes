b4eeb1cc9b7096d6ef1078eeb931f0b7b531d4f5
amir hossein <ahns3.ahe@gmail.com>
2018-09-24 21:36:00 +0330

    Change configurations for standalone cluster

--- PySpark/example.py
+++ PySpark/example.py
@@ -1,5 +1,5 @@
 from pyspark import SparkContext
-sc = SparkContext("local", "example")
+sc = SparkContext("spark://AHNS3:7077", "example")
 logFile = "README.md"
 logData = sc.textFile(logFile).cache()
 numAs = logData.filter(lambda s: 'a' in s).count()
