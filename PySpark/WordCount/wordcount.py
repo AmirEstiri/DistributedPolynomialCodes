ee5c6d89dcaf8f4e1f8849db8fce0f6aaaeaf837
amir hossein <ahns3.ahe@gmail.com>
2018-09-30 21:35:51 +0330

    Wordcount for pyspark apache

---
+++ PySpark/WordCount/wordcount.py
@@ -0,0 +1,5 @@
+text_file = sc.textFile("hdfs://...")
+counts = text_file.flatMap(lambda line: line.split(" ")) \
+             .map(lambda word: (word, 1)) \
+             .reduceByKey(lambda a, b: a + b)
+counts.saveAsTextFile("hdfs://...")
