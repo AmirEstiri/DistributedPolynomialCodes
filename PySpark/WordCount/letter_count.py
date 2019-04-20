9c14ae1dce89b25f34e752db69a43b352d2f2c34
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-04-20 16:36:03 +0430

    Polynomial decode

--- PySpark/WordCount/letter_count.py
+++ PySpark/WordCount/letter_count.py
@@ -4,4 +4,4 @@ logFile = "README.md"
 logData = sc.textFile(logFile).cache()
 numAs = logData.filter(lambda s: 'a' in s).count()
 numBs = logData.filter(lambda s: 'b' in s).count()
-print "Lines with a: %i, lines with b: %i" % (numAs, numBs)
+print ("Lines with a: %i, lines with b: %i" % (numAs, numBs))
