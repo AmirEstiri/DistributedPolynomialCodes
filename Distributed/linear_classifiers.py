c66cbe9989ce75c903fe3f46f2ace9b7b53bfb15
amir hossein <ahns3.ahe@gmail.com>
2019-04-21 17:35:32 +0430

    Clean up & python notebook

--- Distributed/linear_classifiers.py
+++ Distributed/linear_classifiers.py
@@ -89,4 +89,4 @@ class LinearSVM(LinearClassifier):
     """ A subclass that uses the Multiclass SVM loss function """
 
     def loss(self, X_batch, y_batch, reg):
-        return svm_loss_vectorized(self.W, X_batch, y_batch, reg)
+        return svm_distributed_loss_vectorized(self.W, X_batch, y_batch, reg)
