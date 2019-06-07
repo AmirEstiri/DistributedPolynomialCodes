779ea7f34cffc92bfb888fb4e8d4529d1e54bc81
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-06-08 00:30:53 +0430

    Negative numbers

--- Distributed/t.py
+++ Distributed/t.py
@@ -1,5 +1,8 @@
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
+name = MPI.Get_processor_name()
 rank = comm.Get_rank()
-print('My rank is ',rank)
+size = comm.Get_size()
+print("Hello world from processor %s, rank %d out of %d processors\n",
+       name, rank, size)
