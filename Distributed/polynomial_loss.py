0bfcf981e206603d33d436398114cefe03338877
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-06-07 22:07:09 +0430

    Negative numbers

--- Distributed/polynomial_loss.py
+++ Distributed/polynomial_loss.py
@@ -60,39 +60,39 @@ if comm.rank == 0:
 
     cifar10_dir = 'Datasets/cifar-10-batches-py'
 
-    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
-
-    mask = range(num_training, num_training + num_validation)
-    X_val = X_train[mask]
-    y_val = y_train[mask]
-
-    mask = range(num_training)
-    X_train = X_train[mask]
-    y_train = y_train[mask]
-
-    mask = np.random.choice(num_training, num_dev, replace=False)
-    X_dev = X_train[mask]
-    y_dev = y_train[mask]
-
-    mask = range(num_test)
-    X_test = X_test[mask]
-    y_test = y_test[mask]
-
-    X_train = np.reshape(X_train, (X_train.shape[0], -1))
-    X_val = np.reshape(X_val, (X_val.shape[0], -1))
-    X_test = np.reshape(X_test, (X_test.shape[0], -1))
-    X_dev = np.reshape(X_dev, (X_dev.shape[0], -1))
-
-    mean_image = np.mean(X_train, axis=0)
-    X_train -= mean_image
-    X_val -= mean_image
-    X_test -= mean_image
-    X_dev -= mean_image
-
-    X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
-    X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))])
-    X_test = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
-    X_dev = np.hstack([X_dev, np.ones((X_dev.shape[0], 1))])
+    # X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+    #
+    # mask = range(num_training, num_training + num_validation)
+    # X_val = X_train[mask]
+    # y_val = y_train[mask]
+    #
+    # mask = range(num_training)
+    # X_train = X_train[mask]
+    # y_train = y_train[mask]
+    #
+    # mask = np.random.choice(num_training, num_dev, replace=False)
+    # X_dev = X_train[mask]
+    # y_dev = y_train[mask]
+    #
+    # mask = range(num_test)
+    # X_test = X_test[mask]
+    # y_test = y_test[mask]
+    #
+    # X_train = np.reshape(X_train, (X_train.shape[0], -1))
+    # X_val = np.reshape(X_val, (X_val.shape[0], -1))
+    # X_test = np.reshape(X_test, (X_test.shape[0], -1))
+    # X_dev = np.reshape(X_dev, (X_dev.shape[0], -1))
+    #
+    # mean_image = np.mean(X_train, axis=0)
+    # X_train -= mean_image
+    # X_val -= mean_image
+    # X_test -= mean_image
+    # X_dev -= mean_image
+    #
+    # X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
+    # X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))])
+    # X_test = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
+    # X_dev = np.hstack([X_dev, np.ones((X_dev.shape[0], 1))])
 
     print("Running with %d processes:" % comm.Get_size())
 
